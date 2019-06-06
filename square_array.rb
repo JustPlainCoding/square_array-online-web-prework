8db10d086bc8047f70f660c5802b6d0b1a9c62ce
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-06 17:39:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,9 @@
 def square_array(array)
-  array.collect{|num| num * num}
+  squared_array = []
+  array.each{|num| squared_array.push(num * num)}
+  squared_array
 end
 
+def square_with_collect
+  array.collect{|num| num * num}
+end
